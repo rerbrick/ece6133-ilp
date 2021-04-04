501868c882a490b2e48cb62ad8709c25d4c02805
rerbrick <rachelerbrick@gmail.com>
2021-04-04 11:27:27 -0400

    Add file_parser file
    Removed get_file function from main and created file_parser file for functions

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import sys
-import os
-
-###
-# Gets the file name given by the user, adds the directory to the file path,
-# and tries to open the file. Alerts the user if the file does not exist.
-#
-# Expects user to enter only one valid file name.
-###
-def get_file():
-    global benchmark # global variable for reading the file
-    file_name = sys.argv[1] # get name of benchmark file
-    # add benchmark directory to file path
-    file_path = "benchmarks/{}".format(file_name)
-    # try to open the file in read-only mode
-    try:
-        benchmark = open(file_path, "r")
-    except IOError:
-        # the file could not be found
-        print("Error: File does not appear to exist.")
-        return # exit the function
-    print("File was successfully opened.")
-    benchmark.close() # close the file
+from file_parser import * # get all functions from file_parser file
 
 ###
 # Just a main function
