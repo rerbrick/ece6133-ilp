35b03df06d54743e5c648543ed3f3423a8097087
rerbrick <rachelerbrick@gmail.com>
2021-04-04 23:23:49 -0400

    Added function to get block info from benchmark files
    Created read_file method to read the benchmark file line by line and add block information to hard and soft module lists

--- file_parser.py
+++ file_parser.py
@@ -6,6 +6,9 @@
 
 import sys # for command line argument
 
+hard_modules = [] # global list of hard modules
+soft_modules = [] # global list of soft modules
+
 ###
 # Gets the file name given by the user, adds the directory to the file path,
 # and tries to open the file. Alerts the user if the file does not exist.
@@ -40,3 +43,43 @@ def close_file():
         return False # file wasn't successfully closed
     print("Benchmark file was closed.")
     return True # file was closed
+    
+###
+# Reads values from benchmark file and adds them to lists for hard and soft
+# modules
+#
+# Expects files to be written as example in README in benchmarks directory
+###
+def read_file():
+    global benchmark # add benchmark variable to function scope
+    global hard_modules # list for hard module width and height
+    global soft_modules # list for soft module area and area ratio range
+    while True: # iterate through the file
+        line = benchmark.readline() # get one line of benchmark file
+        if len(line) == 0:
+            # line does not have any characters
+            return # exit function
+        # split line into module type and number of modules
+        module = line.strip().split(' - ')
+        if module[0] == 'hard':
+            # following values are hard modules
+            for num in range(int(module[1])):
+                # read the number of hard modules
+                line = benchmark.readline()
+                values = line.strip().split(',') # remove commas
+                # convert string to integers and add to hard modules list
+                temp_list = [int(values[0]), int(values[1])]
+                hard_modules.append(temp_list)
+        elif module[0] == 'soft':
+            # following values are soft modules
+            for num in range(int(module[1])):
+                # read the number of soft modules
+                line = benchmark.readline()
+                values = line.strip().split(',') # remove commas
+                # convert string to integers and add to soft modules list
+                temp_list = [int(values[0]), float(values[1]), float(values[2])]
+                soft_modules.append(temp_list)
+        else:
+            pass # go on to the next line
+                
+            
--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from file_parser import * # get all functions from file_parser file
 # Just a main function
 ###
 def main():
+    global hard_modules, soft_modules
     # try to open the benchmark file
     if not open_file():
         # file could not be opened
         return -1 # return error code
+    read_file() # read the benchmark file
     # try to close the benchmark file
     if not close_file(): 
         # close the benchmark file
