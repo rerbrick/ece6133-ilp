69f181e93d98ac33ce59f0040f84be15e2e5756e
rerbrick <rachelerbrick@gmail.com>
2021-04-14 14:15:05 -0400

    Updated file_generator.py
    Reduced scope of W_max and H_max to just file_generator file; added and tested rotation functionality for hard modules

--- file_generator.py
+++ file_generator.py
@@ -2,8 +2,6 @@
 
 import var
 
-W_max = 0 # maximum chip width
-H_max = 0 # maximum chip height
 global output
 
 ###
@@ -39,17 +37,20 @@ def close_lp():
 ###
 # Calculates the upper bounds of the chip by summing the widths and heights of
 # each model. Assigns the sum of the widths to W_max and the sum of the heights
-# to H_max
+# to H_max. Returns the maximum of the two values
+#
+# @return   the largest of the maximum height and maximum width
 #
 # NOTE: Only considers hard modules for now.
 ###
-def upper_bounds():
-    global W_max, H_max # add upper bounds to function scope
+def upper_bound():
     # @TODO: add soft module functionality
     # sum the widths and the heights of the modules
     max_val = [sum(module) for module in zip(*var.hard_modules)]
     W_max = max_val[0] # maximum width of the chip
     H_max = max_val[1] # maximum height of the chip
+    # return the maximum value between the width and height
+    return max(W_max, H_max)
 
 ###
 # Generates the .lp output file. Writes the necessary statements to the output
@@ -57,46 +58,16 @@ def upper_bounds():
 # constraints, variable type constraints, chip width constraints, and chip
 # height constraints.
 #
-# NOTE: Only considers non-rotated hard modules.
+# NOTE: Only considers rotatable hard modules.
 ###
 def generate_lp():
-    global W_max, H_max # add upper bounds to function scope
     global output # add output file to function scope
-    upper_bounds() # get the upper bounds of the chip
+    
+    bound = upper_bound() # get maximum bound between the height and width
     
     output.write("min: y_star;\n") # try to minimize chip area
     output.write("\n") # new line for readability
     
-    output.write("/* Non-overlap constraints */\n")
-    for mod in range(var.hard_num):
-        # for each hard module
-        m = mod + 1 # number of starting module
-        n = m + 1 # number of subsequent module
-        while n <= var.hard_num:
-            # non-overlap constraint for all subsequent modules
-            tmp = ("x{} + {} <= x{} + {} x{}{} + {} y{}{};"
-                   .format(m, var.hard_modules[m - 1][0], n, W_max, m, n, W_max, 
-                           m, n))
-            # add constraint to output file
-            output.write(tmp + "\n")
-            tmp = ("x{} - {} >= x{} - {} + {} x{}{} - {} y{}{};"
-                   .format(m, var.hard_modules[n - 1][0], n, W_max, W_max, m, 
-                           n, W_max, m, n))
-            # add constraint to output file
-            output.write(tmp + "\n")
-            tmp = ("y{} + {} <= y{} + {} + {} x{}{} - {} y{}{};"
-                   .format(m, var.hard_modules[m - 1][1], n, H_max, H_max, m, n, 
-                           H_max, m, n))
-            # add constraint to output file
-            output.write(tmp + "\n")
-            tmp = ("y{} - {} >= y{} - {} + {} x{}{} + {} y{}{};"
-                   .format(m, var.hard_modules[n - 1][1], n, 2*H_max, H_max, 
-                           m, n, H_max, m, n))
-            # add constraint to output file
-            output.write(tmp + "\n")
-            output.write("\n") # add a new line for readability
-            n = n + 1 # increment n to go to next module
-        
     output.write("/* Chip width and height constraints */\n")
     for mod in range(1, var.hard_num + 1):
         # for each hard module
@@ -106,6 +77,63 @@ def generate_lp():
         output.write("y{} + {} <= y_star;\n".format(mod, var.hard_modules[mod - 1][1]))
     output.write("\n") # add a new line for readability
     
+    output.write("/* Non-overlap constraints */\n")
+    for mod in range(var.hard_num):
+        # for each hard module
+        m = mod + 1 # number of starting module
+        n = m + 1 # number of subsequent module
+        while n <= var.hard_num:
+            # non-overlap constraint for all subsequent modules
+            # x_m + h_m * z_m + w_m(1 - z_m) <= x_n + W_max(x_mn + y_mn)
+            tmp = ("x{} + {} z{} + {} - {} z{} <= x{} + {} x{}{} + {} y{}{};"
+                   .format(m,                             # x_m
+                           var.hard_modules[m - 1][1], m, # h_m * z_m
+                           var.hard_modules[m - 1][0],    # w_m
+                           var.hard_modules[m - 1][0], m, # w_m * z_m
+                           n,                             # x_n
+                           bound, m, n,                   # W_max * x_mn
+                           bound, m, n))                  # W_max * y_mn
+            # add constraint to output file
+            output.write(tmp + "\n")
+            # x_m - h_n * z_n - w_n(1 - z_n) >= x_n - W_max(1 - x_mn + y_mn)
+            tmp = ("x{} - {} z{} - {} + {} z{} >= x{} - {} + {} x{}{} - {} y{}{};"
+                   .format(m,                             # x_m
+                           var.hard_modules[n - 1][1], n, # h_n * z_n
+                           var.hard_modules[n - 1][0],    # w_n
+                           var.hard_modules[n - 1][0], n, # w_n * z_n
+                           n,                             # x_n
+                           bound,                         # W_max
+                           bound, m, n,                   # W_max * x_mn
+                           bound, m, n))                  # W_max * y_mn
+            # add constraint to output file
+            output.write(tmp + "\n")
+            # y_m + h_m * z_m + h_m(1 - z_m) <= y_n + W_max(1 + x_mn - y_mn)
+            tmp = ("y{} + {} z{} + {} - {} z{} <= y{} + {} + {} x{}{} - {} y{}{};"
+                   .format(m,                             # y_m
+                           var.hard_modules[m - 1][0], m, # w_m * z_m
+                           var.hard_modules[m - 1][1],    # h_m
+                           var.hard_modules[m - 1][1], m, # h_m * z_m
+                           n,                             # y_n
+                           bound,                         # W_max
+                           bound, m, n,                   # W_max * x_mn
+                           bound, m, n))                  # W_max * y_mn
+            # add constraint to output file
+            output.write(tmp + "\n")
+            # y_m - w_n * z_n - h_n(1 - z_n) <= y_n + W_max(2 - x_mn - y_mn)
+            tmp = ("y{} - {} z{} - {} + {} z{} >= y{} - {} + {} x{}{} + {} y{}{};"
+                   .format(m,                             # y_m
+                           var.hard_modules[n - 1][0], n, # w_n * z_n
+                           var.hard_modules[n - 1][1],    # h_n
+                           var.hard_modules[n - 1][1], n, # h_n * z_n
+                           n,                             # y_n
+                           2 * bound,                     # 2 * W_max
+                           bound, m, n,                   # W_max * x_mn
+                           bound, m, n))                  # W_max * y_mn
+            # add constraint to output file
+            output.write(tmp + "\n")
+            output.write("\n") # add a new line for readability
+            n = n + 1 # increment n to go to next module
+    
     output.write("/* Variable type constraints */\n")
     # continuous integer constraints
     output.write("int ")
@@ -114,7 +142,12 @@ def generate_lp():
         output.write("x{}, y{}, ".format(mod, mod))
     output.write("y_star;\n") # area needs to be continuous integer
     # binary constraints
-    output.write("bin ")
+    output.write("bin ") # rotation constraints
+    for mod in range(1, var.hard_num):
+        # for each hard module except the last one
+        output.write("z{}, ".format(mod))
+    output.write("z{}; \n".format(var.hard_num))
+    output.write("bin ") # relative position constraints
     for mod in range(1, var.hard_num - 1):
         # for each hard module except the last two
         next_mod = mod + 1 # number of the next module
