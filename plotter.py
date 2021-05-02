ec2173692d528072de06d5fbc900158350e753be
amudhansekar <amudhan1998@gmail.com>
2021-05-02 12:46:31 -0700

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -17,15 +17,19 @@ def plotthing(mylist):
     for n in range(var.hard_num):
         if mylist[3+(5*counter)] == 0:
             rect1=patches.Rectangle((mylist[4+(5*counter)], mylist[5+(5*counter)]), mylist[1+(5*counter)], mylist[2+(5*counter)],facecolor='red',edgecolor='black')
-            #print((mylist[4+(5*counter)], mylist[5+(5*counter)],mylist[1+(5*counter)], mylist[2+(5*counter)])," no rotate")
+            print((mylist[4+(5*counter)], mylist[5+(5*counter)],mylist[1+(5*counter)], mylist[2+(5*counter)])," no rotate")
         else:
             rect1=patches.Rectangle((mylist[4+(5*counter)], mylist[5+(5*counter)]), mylist[2+(5*counter)], mylist[1+(5*counter)],facecolor='yellow',edgecolor='black')
-            #print((mylist[4+(5*counter)], mylist[5+(5*counter)],mylist[2+(5*counter)], mylist[1+(5*counter)])," rotate")
+            print((mylist[4+(5*counter)], mylist[5+(5*counter)],mylist[2+(5*counter)], mylist[1+(5*counter)])," rotate")
         ax1.add_patch(rect1)
         counter = counter+1
-    #counter=0
-    #for n in var.soft_modules:
+
+    for n in range(var.soft_num):
+        rect1=patches.Rectangle((mylist[4+(5*counter)], mylist[5+(5*counter)]), mylist[1+(5*counter)], mylist[2+(5*counter)],facecolor='orange',edgecolor='black')
+        print((mylist[4+(5*counter)], mylist[5+(5*counter)],mylist[1+(5*counter)], mylist[2+(5*counter)])," soft module")
+        ax1.add_patch(rect1)
+        counter = counter+1
     plt.xlim(0,mylist[0])
     plt.ylim(0,mylist[0])
-    #plt.show()
+    plt.show()
 
